255ecd154411983428250c77a46ac2aa10836dbf
UglyGod46 <patlasov.deniska@mail.ru>
2024-11-08 22:09:27 +0700

    Creat static lib for 4.1

---
+++ 6.1/main.c
@@ -0,0 +1,38 @@
+#include "Operations.h"
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("\nContact list:\n");
+        printf("1. Add contact\n");
+        printf("2. Edit contact\n");
+        printf("3. Delete contact\n");
+        printf("4. Show contact list\n");
+        printf("5. Exit\n");
+        printf("Select action: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                AddContact();
+                break;
+            case 2:
+                EditContact();
+                break;
+            case 3:
+                DeleteContact();
+                break;
+            case 4:
+                ListContacts();
+                break;
+            case 5:
+                FreeList();
+                return 0;
+            default:
+                printf("Wrong choice. Try again.\n");
+        }
+    }
+
+    return 0;
+}
